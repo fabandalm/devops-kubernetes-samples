9701bd5056b82b8c45ae117dbe7121af8c562b8a
Almeida <falmeida.tech@gmail.com>
2020-04-23 22:57:56 -0400

    Added new functionality to create docker image.

--- sample-rest-api/src/main/java/com/falmeida/tech/SampleController.java
+++ sample-rest-api/src/main/java/com/falmeida/tech/SampleController.java
@@ -3,9 +3,11 @@ package com.falmeida.tech;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.falmeida.tech.environment.InstanceInformationService;
 
+@RestController
 public class SampleController {
 	
 	@Autowired
@@ -18,7 +20,7 @@ public class SampleController {
 	
 	@GetMapping(path = "/sample")
 	public String helloWorld() {
-		return "Sample " + " V1 " + service.retrieveInstanceInfo();
+		return "Sample " + " V2 " + service.retrieveInstanceInfo();
 	}
 
 	@GetMapping(path = "/sample-bean")
